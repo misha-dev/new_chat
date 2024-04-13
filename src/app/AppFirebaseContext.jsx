cc705162b2cb94fbcf3e1eaa4cb6f0fd2956c246
misha-dev <shabatinmichael@gmail.com>
2024-04-13 22:50:10 +0500

    feature: setting offline to user that closed window tab

--- src/app/AppFirebaseContext.jsx
+++ src/app/AppFirebaseContext.jsx
@@ -1,3 +1,4 @@
+import { setUserOnline } from '@/entities/user/model/setUserOnline';
 import { auth } from '@/shared/firebase/config';
 import { FirebaseContext } from '@/shared/firebase/context';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -5,8 +6,10 @@ import { useEffect, useState } from 'react';
 
 export const AppFirebaseContext = ({ children }) => {
   const [logged, setLogged] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, () => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
       setLogged(true);
     });
 
@@ -14,5 +17,20 @@ export const AppFirebaseContext = ({ children }) => {
       unsubscribe();
     };
   }, []);
+
+  useEffect(() => {
+    const setUserOfflineOnBeforeUnload = () => {
+      if (currentUser) {
+        setUserOnline(currentUser.uid, false);
+      }
+    };
+
+    window.addEventListener('beforeunload', setUserOfflineOnBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', setUserOfflineOnBeforeUnload);
+    };
+  }, [currentUser]);
+
   return <FirebaseContext.Provider value={{ logged }}>{children}</FirebaseContext.Provider>;
 };
