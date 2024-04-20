a9429cb92695688b8c6d5e709efbd691ce47b83a
misha-dev <shabatinmichael@gmail.com>
2024-04-20 19:20:33 +0500

    feature: correct user updating

--- src/entities/user/model/loginUser.js
+++ src/entities/user/model/loginUser.js
@@ -1,17 +1,27 @@
 import { auth, firestore } from '@/shared/firebase/config';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
 
 export const loginUser = async () => {
   const googleProvider = new GoogleAuthProvider();
 
   const { user } = await signInWithPopup(auth, googleProvider);
 
-  await setDoc(doc(firestore, 'users', user.uid), {
-    uid: user.uid,
-    displayName: user.displayName,
-    email: user.email,
-    photoURL: user.photoURL,
-    online: true,
-  });
+  const userDocRef = doc(firestore, 'users', user.uid);
+  const userDocSnapshot = await getDoc(userDocRef);
+
+  if (userDocSnapshot.exists()) {
+    await updateDoc(userDocRef, {
+      online: true,
+    });
+  } else {
+    await setDoc(userDocRef, {
+      uid: user.uid,
+      displayName: user.displayName,
+      email: user.email,
+      photoURL: user.photoURL,
+      online: true,
+      friends: [],
+    });
+  }
 };
